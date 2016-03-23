138b5818434dcc462d5a656196edc0f2bdbc6f48
Tran <CatTran@my.unt.edu>
2016-03-23 11:52:53 -0500

    Declared: input output file object

--- traveling_salesman_problem.cpp
+++ traveling_salesman_problem.cpp
@@ -4,4 +4,28 @@ UNIVERSITY OF NORTH TEXAS || CSCE 2110*/
 
 #include <stio.h>
 #include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	if(argc < 3)
+	{
+		cout << "Too many arguments...";
+		return 0;
+	}
+	else if(argc > 3)
+	{
+		cout << "Too few arguments...";
+		return 0;
+	}
+
+	//declare and open input and output files
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open(argv[1]);
+	outFile.open(argv[2]);
+
+	return 0;
+}
