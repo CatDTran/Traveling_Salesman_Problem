6fb900b06195cc3aa9ade542b272cb3c9ce333a1
Cat Tran <trandinhcat@gmail.com>
2016-03-23 14:07:33 -0600

    Implemented: lineCount, cityCount, pathCount

--- traveling_salesman_problem.cpp
+++ traveling_salesman_problem.cpp
@@ -5,27 +5,53 @@ UNIVERSITY OF NORTH TEXAS || CSCE 2110*/
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-
+#include <string>
 using namespace std;
 
 int main(int argc, char ** argv)
 {
 	if(argc < 3)
 	{
-		cout << "Too many arguments...";
+		cout << "Too few arguments..." << endl;
 		return 0;
 	}
 	else if(argc > 3)
 	{
-		cout << "Too few arguments...";
+		cout << "Too many arguments..." << endl;
 		return 0;
 	}
 
 	//declare and open input and output files
 	ifstream inFile;
 	ofstream outFile;
-	inFile.open(argv[1]);
-	outFile.open(argv[2]);
+	inFile.open(argv[1], ios::in);
+	outFile.open(argv[2], ios::out);
+	if(!inFile.is_open())
+	{
+		cout << "No input file found..." << endl;
+		return 0;
+	}
 
+	//At this state proper arguments and input file are entered
+	int cityCount = 0, pathCount = 0;
+	int lineCount = 0;
+	string dummy;
+	//count number of lines, cities, and paths
+	while(getline(inFile, dummy))
+	{
+		if(dummy[0] == 'c')
+			cityCount++;
+		else if(dummy[0] == 'a')
+			pathCount++;
+		lineCount++;
+		cout << lineCount << "|| cityCount: " << cityCount << "|| pathCount: " << pathCount << endl;
+	}
+	//a string array to store lines
+	string lines[lineCount];
+	int i = 0;
+	while(getline(inFile, lines[i]))
+	{
+		
+	}
 	return 0;
 }
